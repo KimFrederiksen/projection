6bd192f213d522c5fb9373b72d597d33b64aca36
Kim Frederiksen <kim.frederiksen@outlook.com>
2021-05-04 21:26:05 +0000

    finished commit

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
-var N = 65;             // grid divisions
+var gridX = 192;             // grid divisions
+var gridY = 157;
 var SP = 0;             // canvas border
 var DT = 0.1;
 var EPS = 0.00000001;
@@ -19,11 +20,8 @@ var zoney = 100;
 let img;
 
 function preload() {
-	// img = loadImage('https://raw.githubusercontent.com/KimFrederiksen/projection/main/template.png');
-	img = loadImage('https://raw.githubusercontent.com/KimFrederiksen/projection/main/embroidery_bitmap.png');
-
+	img = loadImage('https://raw.githubusercontent.com/KimFrederiksen/projection/main/embroidery_bitmap2.png');
 }
-
 
 function spring_force(ax, ay, bx, by, k) {
     var xx = ax - bx;
@@ -41,8 +39,8 @@ class Dot{
 	constructor(i, j, color){
 		this.vx = 0;
 		this.vy = 0;
-		this.x = map(i, 0, 192-1, SP, width-SP);
-		this.y = map(j, 0, 157-1, SP, height-SP);
+		this.x = map(i, 0, gridX-1, SP, width-SP);
+		this.y = map(j, 0, gridY-1, SP, height-SP);
         this.x0 = this.x;
 		this.y0 = this.y;
 		this.color = color;
@@ -51,14 +49,9 @@ class Dot{
 	update(){
 		let d = dist(zonex, zoney, this.x, this.y);
 		let delta = sDelta;
-		
 		let intensity = sKClick*exp(-d*d/(delta*delta));
-		//float interp = 15*exp(-d/delta);
-    
 		let res = createVector(0, 0);
-		// if(mouseIsPressed){
 		res.add(spring_force(zonex, zoney, this.x, this.y, intensity));
-		// }
 		
 		res.add(spring_force(this.x0, this.y0, this.x, this.y, sKGrid));
 
@@ -79,46 +72,33 @@ function draw_pixel(d1) {
 }
 
 function draw_connection(d1){
-	// stroke(255,125);
 	strokeWeight(0);
 	fill(d1.color);
 	rect(d1.x, d1.y, 1, 1);
-	// line(d1.x, d1.y, d2.x, d2.y);
 }
 
 
 function setup() {
 	pixelDensity(1);
-	var cnv = createCanvas(192, 157);
+	var cnv = createCanvas(gridX, gridY);
 	frameRate(25);
 	noStroke();
 	api.tracking.connect();
-	//cnv.style('width: 100%;margin: 0;justify-content: center;');
     background(0);
 
 	image(img, 0, 0, width, height);
 
-	// let halfImage = 4 * (width * d) * (height * d /2);
-
 	loadPixels();
-	let myimage = 192 * 157;
-
-	// for (let i = 0; i < myimage; i++) {
-	// 	pixels[i + myimage] = pixels[i];
-	// }
-
-	// updatePixels();
-
+	let myimage = gridX * gridY;
 	
-  	array = new Array(192);
-	// array = pixels[];
-	for(let i=0;i<192;i++){
-		array[i] = new Array(157);
+  	array = new Array(gridX);
+	for(let i=0;i<gridX;i++){
+		array[i] = new Array(gridY);
 	}
   
-	for(let i=0;i<192;i++){
-		for(let j=0;j<157;j++){
-			let mypixel = ((j*192) + i) * 4;
+	for(let i=0;i<gridX;i++){
+		for(let j=0;j<gridY;j++){
+			let mypixel = ((j*gridX) + i) * 4;
 			array[i][j] = new Dot(i, j, pixels[mypixel]);
 		}
 	}
@@ -130,42 +110,24 @@ function draw() {
 
 	var blobs = api.tracking.getBlobs();
 	for (var i = 0; i < blobs.length; i++) {
-		// rect(blobs[i].x, blobs[i].y, 22, 22);
 		zonex = blobs[0].x;
 		zoney = blobs[0].y;
 	}
-//	}
 
-	// image(img, 0, 0);
-	//updatePixels();
-	for(let i=0; i<192; i++) {
-		for(let j=0;j<157;j++){
+	for(let i=0; i<gridX; i++) {
+		for(let j=0;j<gridY;j++){
 			array[i][j].update();
 		}
 	}
   
-	for(let i=0;i<192-1;i++){
-		for(let j=0;j<157-1;j++){
+	for(let i=0;i<gridX-1;i++){
+		for(let j=0;j<gridY-1;j++){
 			let d1 = array[i][j];
 			let d2 = array[i+1][j];
 			let d3 = array[i][j+1];
 			draw_connection(d1);
-			// draw_connection(d1, d2);
-			// draw_connection(d1, d3);
-
-			// draw_pixel(d1);
-
 		}
 	}
-	
-	// for(let i=0;i<192-1;i++){
-	// 	let d1 = array[N-1][i];
-	// 	let d2 = array[N-1][i+1];
-	// 	let d3 = array[i][N-1];
-	// 	let d4 = array[i+1][N-1];
-	// 	draw_connection(d1, d2);
-	// 	draw_connection(d3, d4);
-	// }
 	
 	pop();		
 }
